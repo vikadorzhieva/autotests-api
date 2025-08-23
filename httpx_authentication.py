c83d9cdcff04f572defb5b5edfaa443b88ec08c8
Виктория <v.dorzhieva@bnovo.ru>
2025-08-23 13:50:29 +0300

    Используем HTTPX для работы с /api/v1/authentication API

---
+++ httpx_authentication.py
@@ -0,0 +1,28 @@
+import httpx  # Импортируем библиотеку HTTPX
+
+# Инициализируем JSON-данные, которые будем отправлять в API
+login_payload = {
+    "email": "test@mailp.io",
+    "password": "password"
+}
+
+# Выполняем запрос на аутентификацию
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+
+# Выводим полученные токены
+print("Login response:", login_response_data)
+print("Status Code:", login_response.status_code)
+
+# Формируем payload для обновления токена
+refresh_payload = {
+    "refreshToken": login_response_data["token"]["refreshToken"]
+}
+
+# Выполняем запрос на обновление токена
+refresh_response = httpx.post("http://localhost:8000/api/v1/authentication/refresh", json=refresh_payload)
+refresh_response_data = refresh_response.json()
+
+# Выводим обновленные токены
+print("Refresh response:", refresh_response_data)
+print("Status Code:", refresh_response.status_code)
