6c1ec2d8284f9475b8e48d6787ed7f68b88bc290
Виктория <brizyriot@yandex.ru>
2025-08-27 16:31:01 +0300

    Маркировки Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,13 +1,16 @@
 from http import HTTPStatus
 
+import pytest 
+
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-# Импортируем функцию для проверки ответа создания юзера
 from tools.assertions.users import assert_create_user_response
 
 
+@pytest.mark.users  # Добавили маркировку users
+@pytest.mark.regression  # Добавили маркировку regression
 def test_create_user():
     public_users_client = get_public_users_client()
 
@@ -16,7 +19,6 @@ def test_create_user():
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
     assert_status_code(response.status_code, HTTPStatus.OK)
-    # Используем функцию для проверки ответа создания юзера
     assert_create_user_response(request, response_data)
 
     validate_json_schema(response.json(), response_data.model_json_schema())
