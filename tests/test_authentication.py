bbef941023055d310d6112e57a97cfe5141cbd5f
Виктория <brizyriot@yandex.ru>
2025-08-27 16:32:46 +0300

    Практика работы с pytest маркировкам

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,4 +1,6 @@
 from http import HTTPStatus
+
+import pytest
 from clients.authentication.authentication_schema import LoginResponseSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
@@ -8,7 +10,8 @@ from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
-
+@pytest.mark.regression 
+@pytest.mark.authentication
 def test_login():
     public_users_client = get_public_users_client()
     create_user_request = CreateUserRequestSchema()
