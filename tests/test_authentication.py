e88e514e130c28e9f07456b1cdae9ba03351ec44
Виктория <brizyriot@yandex.ru>
2025-08-28 12:06:21 +0300

    Фикстуры Pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,31 +1,30 @@
 from http import HTTPStatus
 
 import pytest
-from clients.authentication.authentication_schema import LoginResponseSchema
-from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.public_users_client import get_public_users_client
-from clients.authentication.authentication_client import get_authentication_client
-from clients.users.users_schema import CreateUserRequestSchema
+
+from clients.authentication.authentication_client import AuthenticationClient
+from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
+from tests.conftest import UserFixture
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
-@pytest.mark.regression 
+
+@pytest.mark.regression
 @pytest.mark.authentication
-def test_login():
-    public_users_client = get_public_users_client()
-    create_user_request = CreateUserRequestSchema()
-    create_user_response = public_users_client.create_user(create_user_request)
+def test_login(
+        function_user: UserFixture,  # Используем фикстуру для создания пользователя
+        authentication_client: AuthenticationClient
+):
+    # Запрос на логин (login_request -> request)
+    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+    # Выполняем логин (login_response -> response)
+    response = authentication_client.login_api(request)
+    # Валидация ответа (login_response_data -> response_data)
+    response_data = LoginResponseSchema.model_validate_json(response.text)
 
-    authentication_client = get_authentication_client()
-    authentication_user = AuthenticationUserSchema(
-        email=create_user_request.email,
-        password=create_user_request.password
-        )
-    login_response = authentication_client.login_api(authentication_user)
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_login_response(response_data)
 
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
-    assert_login_response(login_response_data)
+    validate_json_schema(response.json(), response_data.model_json_schema())
 
-    validate_json_schema(login_response.json(),  login_response_data.model_json_schema())
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,19 +1,18 @@
 from http import HTTPStatus
 
-import pytest 
+import pytest
 
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response
 
 
-@pytest.mark.users  # Добавили маркировку users
-@pytest.mark.regression  # Добавили маркировку regression
-def test_create_user():
-    public_users_client = get_public_users_client()
-
+@pytest.mark.users
+@pytest.mark.regression
+def test_create_user(public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
+    # Удалили инициализацию API клиента из теста
     request = CreateUserRequestSchema()
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
