f7010ffb5ba193e500d5f0d831ca31d1813a8ee1
Виктория <brizyriot@yandex.ru>
2025-09-01 10:22:07 +0300

    Рефакторинг и подготовка к написанию API автотестов

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,28 +1,26 @@
+from functools import lru_cache  # Импортируем функцию для кеширования
+
 from httpx import Client
 from pydantic import BaseModel
 
 from clients.authentication.authentication_client import get_authentication_client
-# Импортируем модель LoginRequestSchema
 from clients.authentication.authentication_schema import LoginRequestSchema
 
 
-# Добавили суффикс Schema вместо Dict
-class AuthenticationUserSchema(BaseModel):  # Наследуем от BaseModel вместо TypedDict
+class AuthenticationUserSchema(BaseModel, frozen=True):  # Добавили параметр frozen=True
     email: str
     password: str
 
 
+@lru_cache(maxsize=None)  # Кешируем возвращаемое значение
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     authentication_client = get_authentication_client()
 
-    # Используем модель LoginRequestSchema
-    # Значения теперь извлекаем не по ключу, а через атрибуты
     login_request = LoginRequestSchema(email=user.email, password=user.password)
     login_response = authentication_client.login(login_request)
 
     return Client(
         timeout=100,
         base_url="http://localhost:8000",
-        # Значения теперь извлекаем не по ключу, а через атрибуты
         headers={"Authorization": f"Bearer {login_response.token.access_token}"}
     )
